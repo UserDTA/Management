0aa5c909343d8a7f771921453ea16c7408f42a2f
Userdt-aisatt <d.tsukamoto@aisatt.jp>
2024-10-10 11:41:58 +0900

    Update chooseForm.vb

--- Management/chooseForm.vb
+++ Management/chooseForm.vb
@@ -1,6 +1,6 @@
 ﻿Public Class chooseForm
     'メインフォーム
-    '管理メニューか勤務表どちらを開くか選択させるフォーム
+    '管理メニューか勤務表どちらを開くか選択するフォーム
     Private Sub btnAdmin_Click(sender As Object, e As EventArgs) Handles btnAdmin.Click
         '管理メニューボタンが押されたとき管理メニューを開く
         Admin.ShowDialog()
