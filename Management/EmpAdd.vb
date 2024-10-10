371e8ed8629c4e08e3d15f77ea01ed6923db96b1
d-tsukamoto <d.tsukamoto@aisatt.jp>
2024-10-10 10:27:03 +0900

    Update EmpAdd.vb

--- Management/EmpAdd.vb
+++ Management/EmpAdd.vb
@@ -1,6 +1,8 @@
 ﻿Imports System.Data.SqlClient
 Public Class EmpAdd
     Private Sub EmpAdd_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        '登録ボタンがクリックされた時の処理
+        'コンボボックスにデータを連結
         cboDep.DataSource = ds
         cboDep.DisplayMember = "部署.部署名"
         cboDep.ValueMember = "部署.部署番号"
@@ -8,28 +10,36 @@ Public Class EmpAdd
 
     Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
         If MessageBox.Show("確定してもよろしいですか？", "確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
-
+            'OKボタンがクリックされた時の処理
+            '新しい社員を登録する
             Try
                 Dim dr As DataRow
                 dr = ds.Tables("社員").NewRow
-                '5-2
+                '編集モード開始
                 dr.BeginEdit()
-                '5-3
+                'テキストボックスに入力された値を取得
                 dr.Item("社員番号") = txtEno.Text
                 dr.Item("氏名") = txtName.Text
                 dr.Item("ふりがな") = txtKana.Text
                 dr.Item("部署番号") = cboDep.SelectedValue
                 dr.Item("役職") = txtPost.Text
                 dr.Item("入社年月日") = txtDate.Text
+                '初期パスワードに「pass」を設定
                 dr.Item("パスワード") = "pass"
-                '5-4
+                '編集モード終了
                 dr.EndEdit()
-                '5-5
+                '社員をテーブルに追加
                 ds.Tables("社員").Rows.Add(dr)
                 Me.Close()
             Catch ex As Exception
+                'エラーの場合はエラーメッセージを表示
                 MessageBox.Show(ex.Message)
             End Try
         End If
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        '閉じるボタンがクリックされた時フォームを閉じる()
+        Me.Close()
     End Sub
 End Class
